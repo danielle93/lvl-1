3ae5b5ec9d4f7893a6b3bf037ac93cad1e5f4560
Danielle Roberson <danielle.roberson@icloud.com>
2017-04-13 09:04:39 -0700

    .

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ function removeTransition(e) {
 
   function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
     if (!audio) return;
 
     key.classList.add('playing');
